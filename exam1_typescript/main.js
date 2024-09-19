2a2b8fe448b1d8d1cb7351f5b320017839ade387
yakov gottlib <yanki5599@gmail.com>
2024-09-19 15:26:49 +0300

    done main.ts, working..

---
+++ exam1_typescript/main.js
@@ -0,0 +1,113 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const form = document.querySelector("form");
+const tableTbody = document.querySelector("tbody");
+const sliders = form.getElementsByTagName("input");
+function submitForm() {
+    const searchParams = extractDataFromForm();
+    postSearch(searchParams);
+}
+function reloadTable(playersList) {
+    clearTable();
+    playersList.forEach((player) => {
+        addPlayerRow(player);
+    });
+}
+function clearTable() {
+    tableTbody.replaceChildren();
+}
+function addPlayerRow(player) {
+    const tr = document.createElement("tr");
+    const playerNameTd = document.createElement("td");
+    const positionTd = document.createElement("td");
+    const twoPercentTd = document.createElement("td");
+    const threePercentTd = document.createElement("td");
+    const pointsTd = document.createElement("td");
+    const actionsTd = document.createElement("td");
+    playerNameTd.textContent = player.playerName;
+    positionTd.textContent = player.position;
+    twoPercentTd.textContent = player.twoPercent.toString() + "%";
+    threePercentTd.textContent = player.threePercent.toString() + "%";
+    pointsTd.textContent = player.points.toString();
+    const button = document.createElement("button");
+    button.textContent = `add ${player.playerName.split(" ")[0]} to current team`;
+    actionsTd.appendChild(button);
+    setAddButtonEL(button, player);
+    tr.append(playerNameTd, positionTd, twoPercentTd, threePercentTd, pointsTd, actionsTd);
+    tableTbody.appendChild(tr);
+}
+function setAddButtonEL(button, player) {
+    button.addEventListener("click", () => {
+        const teamMemberElement = document.getElementById(`${player.position}`);
+        clearPChildren(teamMemberElement);
+        teamMemberElement.append(...createPlayerPElements(player));
+    });
+}
+function createPlayerPElements(player) {
+    const playerNameP = document.createElement("p");
+    const threePercentsP = document.createElement("p");
+    const twoPercentsP = document.createElement("p");
+    const pointsP = document.createElement("p");
+    playerNameP.textContent = player.playerName;
+    threePercentsP.textContent = "Three Percents:" + player.threePercent;
+    twoPercentsP.textContent = "Two Percents:" + player.twoPercent;
+    pointsP.textContent = "Points:" + player.points;
+    return [playerNameP, threePercentsP, twoPercentsP, pointsP];
+}
+function clearPChildren(parent) {
+    const title = parent.firstElementChild;
+    parent.replaceChildren(title);
+}
+function postSearch(searchParams) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const BASE_URL = "https://nbaserver-q21u.onrender.com/api/filter";
+        const options = {
+            headers: { "Content-Type": "application/json" },
+            method: "POST",
+            body: JSON.stringify(searchParams),
+        };
+        try {
+            const response = yield fetch(BASE_URL, options);
+            if (!response.ok) {
+                throw new Error(`error fetching data from server. status:${response.status}`);
+            }
+            const playersList = (yield response.json());
+            console.log(`playerlist`, playersList);
+            reloadTable(playersList);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    });
+}
+function extractDataFromForm() {
+    const formValues = {};
+    formValues.position = form["position"].value;
+    formValues.twoPercent = +form["twoPercent"].value;
+    formValues.threePercent = +form["threePercent"].value;
+    formValues.points = +form["points"].value;
+    return formValues;
+}
+window.onload = () => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        console.log("submit evfent");
+        submitForm();
+    });
+    for (const slider of sliders) {
+        slider.addEventListener("input", () => {
+            var _a;
+            const theBlackLabel = (_a = slider.parentElement) === null || _a === void 0 ? void 0 : _a.querySelector(".sliderLabel");
+            theBlackLabel.textContent = slider.value.toString();
+            slider.title = slider.value.toString();
+        });
+    }
+};
--- exam1_typescript/main.ts
+++ exam1_typescript/main.ts
@@ -1,13 +1,5 @@
-enum PlayerPosition {
-  PG = "PG",
-  SG = "SG",
-  SF = "SF",
-  PF = "PF",
-  C = "C",
-}
-
 interface Player {
-  _id?: string;
+  //   _id?: string;
   position: string;
   playerName: string;
   twoPercent: number;
@@ -16,26 +8,84 @@ interface Player {
 }
 
 const form: HTMLFormElement = document.querySelector("form") as HTMLFormElement;
-const tableTbody: HTMLElement = document.querySelector(
-  "form tbody"
-) as HTMLElement;
+const tableTbody: HTMLElement = document.querySelector("tbody") as HTMLElement;
+
+const sliders = form.getElementsByTagName(
+  "input"
+) as HTMLCollectionOf<HTMLInputElement>;
 
 function submitForm(): void {
   const searchParams: Partial<Player> = extractDataFromForm();
 
   postSearch(searchParams);
 }
-
 function reloadTable(playersList: Player[]): void {
+  clearTable();
   playersList.forEach((player) => {
     addPlayerRow(player);
   });
 }
-
+function clearTable(): void {
+  tableTbody.replaceChildren();
+}
 function addPlayerRow(player: Player): void {
   const tr = document.createElement("tr") as HTMLTableRowElement;
-}
 
+  const playerNameTd = document.createElement("td") as HTMLTableCellElement;
+  const positionTd = document.createElement("td") as HTMLTableCellElement;
+  const twoPercentTd = document.createElement("td") as HTMLTableCellElement;
+  const threePercentTd = document.createElement("td") as HTMLTableCellElement;
+  const pointsTd = document.createElement("td") as HTMLTableCellElement;
+  const actionsTd = document.createElement("td") as HTMLTableCellElement;
+
+  playerNameTd.textContent = player.playerName;
+  positionTd.textContent = player.position;
+  twoPercentTd.textContent = player.twoPercent.toString() + "%";
+  threePercentTd.textContent = player.threePercent.toString() + "%";
+  pointsTd.textContent = player.points.toString();
+
+  const button: HTMLButtonElement = document.createElement("button");
+  button.textContent = `add ${player.playerName.split(" ")[0]} to current team`;
+  actionsTd.appendChild(button);
+  setAddButtonEL(button, player);
+
+  tr.append(
+    playerNameTd,
+    positionTd,
+    twoPercentTd,
+    threePercentTd,
+    pointsTd,
+    actionsTd
+  );
+  tableTbody.appendChild(tr);
+}
+function setAddButtonEL(button: HTMLButtonElement, player: Player): void {
+  button.addEventListener("click", () => {
+    const teamMemberElement = document.getElementById(
+      `${player.position}`
+    ) as HTMLDivElement;
+    clearPChildren(teamMemberElement);
+    teamMemberElement.append(...createPlayerPElements(player));
+  });
+}
+function createPlayerPElements(player: Player): HTMLParagraphElement[] {
+  const playerNameP = document.createElement("p") as HTMLParagraphElement;
+  const threePercentsP = document.createElement("p") as HTMLParagraphElement;
+  const twoPercentsP = document.createElement("p") as HTMLParagraphElement;
+  const pointsP = document.createElement("p") as HTMLParagraphElement;
+
+  playerNameP.textContent = player.playerName;
+  threePercentsP.textContent = "Three Percents:" + player.threePercent;
+  twoPercentsP.textContent = "Two Percents:" + player.twoPercent;
+  pointsP.textContent = "Points:" + player.points;
+
+  return [playerNameP, threePercentsP, twoPercentsP, pointsP];
+}
+function clearPChildren(parent: HTMLDivElement): void {
+  const title: HTMLHeadingElement =
+    parent.firstElementChild as HTMLHeadingElement;
+  parent.replaceChildren(title);
+}
 async function postSearch(searchParams: Partial<Player>): Promise<void> {
   const BASE_URL = "https://nbaserver-q21u.onrender.com/api/filter";
 
@@ -54,14 +104,14 @@ async function postSearch(searchParams: Partial<Player>): Promise<void> {
       );
     }
 
-    const playersList = response.json() as unknown as Player[];
+    const playersList = (await response.json()) as unknown as Player[];
+    console.log(`playerlist`, playersList);
 
     reloadTable(playersList);
   } catch (err: any) {
     console.error(err);
   }
 }
-
 function extractDataFromForm(): Partial<Player> {
   const formValues: Partial<Player> = {};
   formValues.position = form["position"].value;
@@ -70,10 +120,19 @@ function extractDataFromForm(): Partial<Player> {
   formValues.points = +form["points"].value;
   return formValues;
 }
-
 window.onload = () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log("submit evfent");
     submitForm();
   });
+
+  for (const slider of sliders) {
+    slider.addEventListener("input", () => {
+      const theBlackLabel: HTMLSpanElement =
+        slider.parentElement?.querySelector(".sliderLabel") as HTMLSpanElement;
+      theBlackLabel.textContent = slider.value.toString();
+      slider.title = slider.value.toString();
+    });
+  }
 };
