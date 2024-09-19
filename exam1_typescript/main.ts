41b602ebec3e8a9fcc05521f6e6e7fbf1dacb2f7
yakov gottlib <yanki5599@gmail.com>
2024-09-19 17:35:59 +0300

    added documentation

--- exam1_typescript/main.ts
+++ exam1_typescript/main.ts
@@ -34,20 +34,27 @@ const currentTeam: Team = {
 
 let timer: number | null;
 
+// search for matching players
 function submitForm(): void {
   const searchParams: Partial<Player> = extractDataFromForm();
 
   postSearch(searchParams);
 }
+
+// gets a new list of players and refills the table with them
 function reloadTable(playersList: Player[]): void {
   clearTable();
   playersList.forEach((player) => {
     addPlayerRow(player);
   });
 }
+
+// clears tbody trs of table
 function clearTable(): void {
   tableTbody.replaceChildren();
 }
+
+// adds a tr in body with player info
 function addPlayerRow(player: Player): void {
   const tr = document.createElement("tr") as HTMLTableRowElement;
 
@@ -79,6 +86,8 @@ function addPlayerRow(player: Player): void {
   );
   tableTbody.appendChild(tr);
 }
+
+// sets the event listener for individual button of player row
 function setAddButtonEL(button: HTMLButtonElement, player: Player): void {
   button.addEventListener("click", () => {
     const teamMemberElement = document.getElementById(
@@ -89,6 +98,8 @@ function setAddButtonEL(button: HTMLButtonElement, player: Player): void {
     (currentTeam as any)[player.position].player = player;
   });
 }
+
+// creates the paragraph elements for the current team view
 function createPlayerPElements(player: Player): HTMLParagraphElement[] {
   const playerNameP = document.createElement("p") as HTMLParagraphElement;
   const threePercentsP = document.createElement("p") as HTMLParagraphElement;
@@ -102,11 +113,15 @@ function createPlayerPElements(player: Player): HTMLParagraphElement[] {
 
   return [playerNameP, threePercentsP, twoPercentsP, pointsP];
 }
+
+// a general function that remove all paragraph elements from a parent element
 function clearPChildren(parent: HTMLDivElement): void {
   const title: HTMLHeadingElement =
     parent.firstElementChild as HTMLHeadingElement;
   parent.replaceChildren(title);
 }
+
+// fetches players data with post request
 async function postSearch(searchParams: Partial<Player>): Promise<void> {
   const BASE_URL = "https://nbaserver-q21u.onrender.com/api/filter";
 
@@ -135,6 +150,8 @@ async function postSearch(searchParams: Partial<Player>): Promise<void> {
     showErrorMsg(err);
   }
 }
+
+// creates a player object with form input values
 function extractDataFromForm(): Partial<Player> {
   const formValues: Partial<Player> = {};
   formValues.position = form["position"].value;
@@ -144,6 +161,7 @@ function extractDataFromForm(): Partial<Player> {
   return formValues;
 }
 
+// modal for showing errors with timer
 function showErrorMsg(msg: string): void {
   const msgDivElement = document.createElement("div") as HTMLDivElement;
   msgDivElement.textContent = msg;
@@ -155,6 +173,7 @@ function showErrorMsg(msg: string): void {
   }, 3000);
 }
 
+// modal for showing success messages with timer
 function showSuccessMsg(msg: string): void {
   const msgDivElement = document.createElement("div") as HTMLDivElement;
   msgDivElement.textContent = msg;
@@ -165,6 +184,8 @@ function showSuccessMsg(msg: string): void {
     msgDivElement.remove();
   }, 3000);
 }
+
+// adding team with post request
 async function addTeam(): Promise<void> {
   const BASE_URL: string = "https://nbaserver-q21u.onrender.com/api/AddTeam";
 
@@ -192,6 +213,16 @@ async function addTeam(): Promise<void> {
   }
 }
 
+// returns a list of players in the current team
+function getMyTeam(): Player[] {
+  const res: Player[] = [];
+  Object.keys(currentTeam).forEach((key: string) => {
+    res.push((currentTeam as any)[key].player);
+  });
+  return res;
+}
+
+// setting things up when page reloads
 window.onload = () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -209,11 +240,3 @@ window.onload = () => {
 
   addTeamBtn.addEventListener("click", addTeam);
 };
-
-function getMyTeam(): Player[] {
-  const res: Player[] = [];
-  Object.keys(currentTeam).forEach((key: string) => {
-    res.push((currentTeam as any)[key].player);
-  });
-  return res;
-}
